fac74d7cc8a093ee4afb698f7d37513332265a47
Vedha varshini S <varshinivenketmukesh@gmail.com>
2026-06-26 14:09:23 +0530

    Implement maximumWealth method to calculate wealth

---
+++ 1672_Richest_Customer_Wealth.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumWealth(self, accounts):
+        maximum = 0
+        for customer in accounts:
+            wealth = sum(customer)
+            if wealth > maximum:
+                maximum = wealth
+        return maximum
